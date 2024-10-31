3d3f61ef2ba35dcfcf29b20d95550b0cc93897e2
Mohammed Shamil <kmshamil123@gmail.com>
2024-10-31 14:41:27 +0530

    Built a new issue page

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,15 @@
+import { Button, TextArea, TextField } from "@radix-ui/themes";
+import React from "react";
+import { FaMagnifyingGlass } from "react-icons/fa6";
+
+function NewIssuePage() {
+  return (
+    <div className="max-w-xl space-y-3">
+      <TextField.Root placeholder="Title"></TextField.Root>
+      <TextArea placeholder="Description"></TextArea>
+      <Button>Submit</Button>
+    </div>
+  );
+}
+
+export default NewIssuePage;
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Button } from '@radix-ui/themes'
+import Link from 'next/link'
 
 function page() {
   return (
     <div>
-        <Button>New issue</Button>
+        <Button><Link href='/issues/new'>New Issue</Link></Button>
     </div>
   )
 }
